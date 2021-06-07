1234a632972b27ec4a13009c1368af57201aa624
olga20011 <olgaestherakello@gmail.com>
2021-06-07 20:27:53 +0300

    changes

--- bank.py
+++ bank.py
@@ -41,10 +41,46 @@ class Account:
         return self.show_balance()    
 
     def borrow(self,amount):
-        return  f"Your account has been topped up with {amount}"
+        if amount < 0:
+            return"The amount should be more than 0"
+        
+        elif self.loan > 0:
+            return f"Please repay your outstanding loan"
+            
+        elif amount >= 0.1*self.balance :
+            return f"Your not qualified" 
+        else:
+            loan = amount *1.05
+            self.loan=loan
+            self.balance=+amount
+            now=datetime.now()
+            borrow={
+            "amount":amount,
+            "time":now,
+            "narration":"Your account has been topped up with"}
+            self.statement.append(borrow)
+
+        return  f"Your account has now been topped up with {amount}"
 
     def pay(self,amount):
-        return  f"You have repaid {amount}"   
+        if amount <0:
+            return f"The amount must be grater than 0"
+        elif amount < self.loan:
+            self.loan-=amount
+            return f" You have paid part of your loan with {amount} and {self.loan} is left" 
+        else:
+            diff=amount-self.loan
+            self.loan=0
+            self.deposit(diff)
+            now=datetime.now()
+            pay={
+                "amount":amount,
+                "time":now,
+                "narration":"You have paid"
+                
+            }    
+            self.statement.append(pay)
+        return  f"You have cleared your loan and your account balance is {diff}"   
 
     def Show_statement(self):
         for transaction in self.statement:
@@ -53,6 +89,7 @@ class Account:
                 time=transaction["time"]
                 date=time.strftime("%d/%m/%y")
                 print(f"{date}:{narration} {amount}")      
-        return   
+        return  
+
 
         
